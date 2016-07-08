56a765005f65b4c9749a1e0f8e103f1dd5e723a9
jyh <1048922801@qq.com>
2016-07-08 13:32:17 +0800

    测试版本管理是否关联上

--- app/src/main/java/yhjia/com/circle/activity/MainActivity.java
+++ app/src/main/java/yhjia/com/circle/activity/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends BaseActivity {
     private TimePickerUtil timePickerUtil;
     private View selectNumber;
     private OptionPickerUtil numberPickerUtil;
+    
     private ViewGroup group;
 
 
